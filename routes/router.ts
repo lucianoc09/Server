990ac422414961bca70e4ad488bd894fbdffd475
lucianoc09 <corteggianoluciano@gmail.com>
2022-06-15 13:53:45 -0300

    nuevos cambios

--- routes/router.ts
+++ routes/router.ts
@@ -12,10 +12,27 @@ router.get('/socies', (req: Request, res: Response) => {
 
 router.post('/registro', (req: Request, res: Response) => {
 
-    const nombre = req.body.nombre;
+    let nombre: string;
+    let apellido: string;
+
+    /**
+     * Valido el campo nombre
+     */
+    if (req.body.nombre && req.body.nombre !== ""){
+        nombre = req.body.nombre;
+    }
+    else{
+        res.json({
+            ok: false,
+            mensaje: `Error: Falta el campo nombre`
+        });
+        nombre = "";
+    }
+
+    apellido = req.body.apellido?req.body.apellido:"";
 
     res.json({
         ok: true,
-        mensaje: `Bienvenido ${nombre} !!!`
+        mensaje: `Bienvenido ${nombre} ${apellido} !!!`
     });
 });
